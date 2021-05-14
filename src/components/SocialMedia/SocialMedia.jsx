792bbe28d9e79f2838e275e6ef28b229c7eba073
arifur-ra <ararif_017@yahoo.com>
2021-05-14 19:38:34 +0200

    update file

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -6,16 +6,28 @@ import React from "react";
 const SocialMedia = () => {
   return (
     <div className="link-icon">
-      <a href="https://github.com/arifur-ra" className="social-icon">
+      <a
+        href="https://github.com/arifur-ra"
+        target="_blank"
+        className="social-icon"
+        rel="noreferrer"
+      >
         <GitHubIcon />
       </a>
       <a
         href="https://www.linkedin.com/in/arifur-rahman-arif-416735131/"
+        target="_blank"
         className="social-icon"
+        rel="noreferrer"
       >
         <LinkedInIcon />
       </a>
-      <a href="https://twitter.com/?lang=de" className="social-icon">
+      <a
+        href="https://twitter.com/?lang=de"
+        target="_blank"
+        className="social-icon"
+        rel="noreferrer"
+      >
         <TwitterIcon />
       </a>
     </div>
