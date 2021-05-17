3f05f70bea12a3783811a3553776ae06c1e5d9ee
arifur-ra <ararif_017@yahoo.com>
2021-05-17 12:56:33 +0200

    update portpholoo

--- src/components/Project/ProjectContainer.jsx
+++ src/components/Project/ProjectContainer.jsx
@@ -2,53 +2,48 @@ import React from "react";
 import { Element } from "react-scroll";
 import Lavilla from "../../assets/projects/lavilla.png";
 import ToDoApp from "../../assets/projects/ToDoapp.png";
+import todoApp from "../../assets/projects/todoApp.png";
 import Project from "./Project";
 import "./ProjectContainer.css";
 
 const ProjectContainer = () => {
   const projects = [
     {
-      img: Lavilla,
-      title: "La-Villa",
-      link:
-        "https://arifur-ra.github.io/My_WebDev/HTML&CSS/Final_Lavilla/public/index.html",
-      desc: "using HTML CSS SASS Technology",
+      img: todoApp,
+      title: "todoApp",
+      link: "https://arifur-ra.github.io/todoApp/",
+      desc: "using HTML CSS JavaScript, React, Material-UI Technology",
     },
     {
       img: ToDoApp,
       title: "ToDoApp",
-      link:
-        "https://arifur-ra.github.io/My_WebDev/Java_Script/11_Local_Storae/localstorage/localstorage.html",
+      link: "https://arifur-ra.github.io/My_WebDev/Java_Script/11_Local_Storae/localstorage/localstorage.html",
       desc: "using HTML CSS JavaScript Technology",
     },
     {
       img: Lavilla,
       title: "La-Villa",
-      link:
-        "https://arifur-ra.github.io/My_WebDev/HTML&CSS/Final_Lavilla/public/index.html",
+      link: "https://arifur-ra.github.io/My_WebDev/HTML&CSS/Final_Lavilla/public/index.html",
+      desc: "using HTML CSS SASS Technology",
+    },
+
+    {
+      img: todoApp,
+      title: "todoApp",
+      link: "https://arifur-ra.github.io/todoApp/",
+      desc: "using HTML CSS JavaScript, React, Material-UI Technology",
+    },
+    {
+      img: Lavilla,
+      title: "La-Villa",
+      link: "https://arifur-ra.github.io/My_WebDev/HTML&CSS/Final_Lavilla/public/index.html",
       desc: "using HTML CSS SASS Technology",
     },
 
     {
       img: ToDoApp,
       title: "ToDoApp",
-      link:
-        "https://arifur-ra.github.io/My_WebDev/Java_Script/11_Local_Storae/localstorage/localstorage.html",
-      desc: "using HTML CSS JavaScript Technology",
-    },
-    {
-      img: Lavilla,
-      title: "La-Villa",
-      link:
-        "https://arifur-ra.github.io/My_WebDev/HTML&CSS/Final_Lavilla/public/index.html",
-      desc: "using HTML CSS SASS Technology",
-    },
-
-    {
-      img: ToDoApp,
-      title: "ToDoApp",
-      link:
-        "https://arifur-ra.github.io/My_WebDev/Java_Script/11_Local_Storae/localstorage/localstorage.html",
+      link: "https://arifur-ra.github.io/My_WebDev/Java_Script/11_Local_Storae/localstorage/localstorage.html",
       desc: "using HTML CSS JavaScript Technology",
     },
   ];
