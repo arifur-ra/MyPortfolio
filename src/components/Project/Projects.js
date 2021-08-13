a3c7747c09a4b8d50fab72fe844a460c287e2c8d
arifur-ra <ararif_017@yahoo.com>
2021-08-13 12:31:07 +0200

    project

--- src/components/Project/Projects.js
+++ src/components/Project/Projects.js
@@ -1,4 +1,7 @@
 import lavilla from "../../assets/projects/lavilla.png";
+import menuItem from "../../assets/projects/menu-item.png";
+import modal from "../../assets/projects/modal.png";
+import reviewCard from "../../assets/projects/review-card.png";
 import todoApp from "../../assets/projects/todoApp.png";
 import ToDoApp from "../../assets/projects/ToDoapp.png";
 
@@ -20,19 +23,19 @@ const projects = [
       "https://github.com/arifur-ra/Project_Using-_JS/tree/main/ToDoList",
   },
   {
-    img: todoApp,
-    title: "todoApp",
-    link: "https://arifur-ra.github.io/todoApp/",
-    desc: "using HTML CSS JavaScript, React, Material-UI Technology",
-    githubLink: "https://github.com/arifur-ra/todoApp",
-  },
-  {
-    img: ToDoApp,
-    title: "ToDoApp",
-    link: "https://arifur-ra.github.io/My_WebDev/Java_Script/11_Local_Storae/localstorage/localstorage.html",
+    img: menuItem,
+    title: "Menu List",
+    link: "https://arifur-ra.github.io/MyProject/10_menu-item/index.html",
     desc: "using HTML CSS JavaScript Technology",
     githubLink:
-      "https://github.com/arifur-ra/Project_Using-_JS/tree/main/ToDoList",
+      "https://github.com/arifur-ra/MyProject/tree/master/10_menu-item",
+  },
+  {
+    img: reviewCard,
+    title: "Review-card",
+    link: "https://arifur-ra.github.io/MyProject/05_reviews/index.html",
+    desc: "using HTML CSS JavaScript Technology",
+    githubLink: "https://github.com/arifur-ra/MyProject/tree/master/05_reviews",
   },
   {
     img: todoApp,
@@ -42,12 +45,11 @@ const projects = [
     githubLink: "https://github.com/arifur-ra/todoApp",
   },
   {
-    img: lavilla,
-    title: "La-Villa",
-    link: "https://arifur-ra.github.io/My_WebDev/HTML&CSS/Final_Lavilla/public/index.html",
+    img: modal,
+    title: "Modal",
+    link: "https://arifur-ra.github.io/MyProject/08_modal/index.html",
     desc: "using HTML CSS SASS Technology",
-    githubLink:
-      "https://github.com/arifur-ra/My_WebDev/tree/master/HTML%26CSS/Final_Lavilla",
+    githubLink: "https://github.com/arifur-ra/MyProject/tree/master/08_modal",
   },
 ];
 export default projects;
